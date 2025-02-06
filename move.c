94cab87c3f9da29615ceb6cc5c847eb2a0185d42
ilyasguney <ilyasguney105@gmail.com>
2025-02-06 17:44:40 +0300

    move.c

--- move.c
+++ move.c
@@ -5,39 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: iguney <iguney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/05 04:07:51 by iguney            #+#    #+#             */
-/*   Updated: 2025/02/05 05:36:06 by iguney           ###   ########.fr       */
+/*   Created: 2025/02/05 07:00:37 by iguney            #+#    #+#             */
+/*   Updated: 2025/02/06 17:11:42 by iguney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void flood_fill(t_data *data)
+void flood_fill(t_data *data, int y, int x)
 {
-    if(data->map[data->player->y][data->player->x] != '1' && marker())
-        data->map[data->player->y][data->player->x] = 'X';
+    x = data->player->x;
+    y = data->player->y; 
+    if(data->map_reachable[y][x] != '1')
+        data->map_reachable[y][x] = 'X';
+    if (data->map_reachable[y][x] == '1' 
+        || data->map_reachable[y][x] == 'X')
+		return ;
     
-    flood_fill(++data->player->y);
-    flood_fill(++data->player->x);
-    flood_fill(--data->player->y);
-    flood_fill(--data->player->x);
+    // flood_fill(y + 1);
+    // flood_fill(x + 1);
+    // flood_fill(y - 1);
+    // flood_fill(x - 1);
 }
 
-void marker(t_data *data)
+void check_reachable(t_data *data)
 {
-    int i;
-    int j;
-
-    i = 0;
-    j = 0;
+    data->i = -1;
+	data->j = -1;
     while (++data->i < data->vertical)
     {	
         data->j = -1;
         while (++data->j < data->horizontal)
         {
-            if (data->map[i][j] != '1')
-                data->map[i][j] = 'X';
+            if (data->map_reachable[data->i][data->j] != '1' 
+				&& data->map_reachable[data->i][data->j] != 'X')
+				return (free(data->map_reachable), error("All character must be reachable!"));
         }
     }
 }
-
