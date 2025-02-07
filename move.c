6f5458e0a04136fd50f338c721147e39ac0351fd
ilyasguney <ilyasguney105@gmail.com>
2025-02-07 19:14:54 +0300

    move.c

--- move.c
+++ move.c
@@ -6,40 +6,40 @@
 /*   By: iguney <iguney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 07:00:37 by iguney            #+#    #+#             */
-/*   Updated: 2025/02/06 17:11:42 by iguney           ###   ########.fr       */
+/*   Updated: 2025/02/07 19:06:26 by iguney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void flood_fill(t_data *data, int y, int x)
+void flood_fill(t_data *data, int x, int y)
 {
-    x = data->player->x;
-    y = data->player->y; 
-    if(data->map_reachable[y][x] != '1')
-        data->map_reachable[y][x] = 'X';
-    if (data->map_reachable[y][x] == '1' 
-        || data->map_reachable[y][x] == 'X')
+	if (y < 0 || y >= data->vertical || x < 0 || x >= data->horizontal)
 		return ;
-    
-    // flood_fill(y + 1);
-    // flood_fill(x + 1);
-    // flood_fill(y - 1);
-    // flood_fill(x - 1);
+	if (data->map_reachable[y][x] == '1' || data->map_reachable[y][x] == 'X')
+		return ;
+
+	data->map_reachable[y][x] = 'X';
+	flood_fill(data, x, y + 1);
+	flood_fill(data, x, y - 1);
+	flood_fill(data, x + 1, y);
+	flood_fill(data, x - 1, y);
 }
 
 void check_reachable(t_data *data)
 {
-    data->i = -1;
-	data->j = -1;
-    while (++data->i < data->vertical)
-    {	
-        data->j = -1;
-        while (++data->j < data->horizontal)
-        {
-            if (data->map_reachable[data->i][data->j] != '1' 
-				&& data->map_reachable[data->i][data->j] != 'X')
-				return (free(data->map_reachable), error("All character must be reachable!"));
-        }
-    }
+	int i, j;
+
+	j = 0;
+	while (j < data->vertical)
+	{
+		i = 0;
+		while (i < data->horizontal)
+		{
+			if (data->map_reachable[j][i] != '1' && data->map_reachable[j][i] != 'X')
+				return(free(data->map_reachable), error("The map has an unreachable point!"));
+			i++;
+		}
+		j++;
+	}
 }
